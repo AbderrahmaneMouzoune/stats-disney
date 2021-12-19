1493f4c7527f0baf01193cb7ffcaa279ef9ab177
Abderrahmane Mouzoune <abderrahmane_mouzoune@carrefour.com>
2021-12-19 21:59:09 +0100

    add data and model

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import './Styles/common.scss'
 import SideBar from './Layout/Sidebar/Sidebar'
 import { Col, Container, Row } from 'reactstrap'
 import TotalSale from './Components/TotalSale/TotalSale'
+import { MDBDataTableV5 } from 'mdbreact'
+import Sales from '../static/data.json'
+import { useState } from 'react'
 
 function App() {
+
+    const [dataTable, setDataTable] = useState(Sales)
+
     return (
         <>
             <SideBar />
@@ -19,6 +25,11 @@ function App() {
                     <Row>
                         <Col xl={4} md={12}>
                             <TotalSale />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={dataTable} />;
                         </Col>
                     </Row>
                 </Container>
